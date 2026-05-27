71d59f7fb2e430564a31feda029cbd1b605f7a98
BenSiv <bensiv92@gmail.com>
2026-05-27 15:38:30 -0700

    Added init command and settings.json lookup to load default password store automatically

--- entry.lua
+++ entry.lua
@@ -1,28 +1,77 @@
 package.path = package.path .. ";./?.lua;src/?.lua"
 require("obfuscator")
 
+-- Helper: Retrieve and validate default store path from settings.json if not passed explicitly
+function get_store_path(passed_path)
+    if passed_path != nil and passed_path != "" then
+        return resolve_path(passed_path)
+    end
+    
+    settings_file = resolve_path("~/.config/camopass/settings.json")
+    f = io.open(settings_file, "r")
+    if f == nil then
+        print("Error: No store path provided, and ~/.config/camopass/settings.json does not exist.")
+        print("Please run 'camopass init <store_path>' first to set a default store.")
+        os.exit(false)
+    end
+    io.input(f)
+    content = io.read("*a")
+    io.close(f)
+    
+    default_store = string.match(content, '"default_store"%s*:%s*"([^"]+)"')
+    if default_store == nil then
+        print("Error: Invalid settings.json configuration. Please run 'camopass init <store_path>' to reinitialize.")
+        os.exit(false)
+    end
+    return resolve_path(default_store)
+end
+
 -- Print usage instructions using Luam's triple-quoted strings
 function usage()
     print("""
 CamoPass - Secure Metadata Obfuscator for Pass (MIT License)
 
 Usage:
+  camopass init <store_path>
   camopass hide <source_store> <target_store> <gpg_id>
-  camopass list <store_path>
-  camopass show <store_path> <entry_name>
-  camopass clip <store_path> <entry_name>
+  camopass list [<store_path>]
+  camopass show [<store_path>] <entry_name>
+  camopass clip [<store_path>] <entry_name>
 """)
     os.exit(false)
 end
 
 args = arg
-if #args < 2 then
+if #args < 1 then
     usage()
 end
 
 cmd = args[1]
 
-if cmd == "hide" then
+if cmd == "init" then
+    if #args != 2 then
+        print("Error: init requires <store_path>")
+        os.exit(false)
+    end
+    store_path = resolve_path(args[2])
+    
+    -- Ensure configuration directory exists
+    config_dir = resolve_path("~/.config/camopass")
+    os.execute("mkdir -p " .. string.format("%q", config_dir))
+    
+    -- Write settings.json
+    settings_file = config_dir .. "/settings.json"
+    f = io.open(settings_file, "w")
+    if f == nil then
+        print("Error: Could not write settings to " .. settings_file)
+        os.exit(false)
+    end
+    io.output(f)
+    io.write('{\n  "default_store": "' .. store_path .. '"\n}\n')
+    io.close(f)
+    print("Successfully initialized default store path: " .. store_path)
+
+elseif cmd == "hide" then
     if #args != 4 then
         print("Error: hide requires <source_store> <target_store> <gpg_id>")
         os.exit(false)
@@ -35,7 +84,13 @@ if cmd == "hide" then
     print(err)
     
 elseif cmd == "list" then
-    store_path = args[2]
+    store_path = nil
+    if #args >= 2 then
+        store_path = get_store_path(args[2])
+    else
+        store_path = get_store_path(nil)
+    end
+    
     mappings = load_index(store_path)
     if mappings == nil then
         print("Error: Could not load index.gpg from " .. store_path)
@@ -57,12 +112,18 @@ elseif cmd == "list" then
     end
 
 elseif cmd == "show" then
-    if #args != 3 then
-        print("Error: show requires <store_path> <entry_name>")
+    store_path = nil
+    entry_name = nil
+    if #args == 2 then
+        store_path = get_store_path(nil)
+        entry_name = args[2]
+    elseif #args == 3 then
+        store_path = get_store_path(args[2])
+        entry_name = args[3]
+    else
+        print("Error: show requires <entry_name> [or <store_path> <entry_name>]")
         os.exit(false)
     end
-    store_path = args[2]
-    entry_name = args[3]
     
     mappings = load_index(store_path)
     if mappings == nil then
@@ -85,12 +146,17 @@ elseif cmd == "show" then
     io.write(decrypted)
 
 elseif cmd == "clip" then
-    if #args != 3 then
-        print("Error: clip requires <store_path> <entry_name>")
+    store_path = nil
+    entry_name = nil
+    if #args == 2 then
+        store_path = get_store_path(nil)
+        entry_name = args[2]
+    elseif #args == 3 then
+        store_path = get_store_path(args[2])
+        entry_name = args[3]
+    else
         os.exit(false)
     end
-    store_path = args[2]
-    entry_name = args[3]
     
     mappings = load_index(store_path)
     if mappings == nil then
